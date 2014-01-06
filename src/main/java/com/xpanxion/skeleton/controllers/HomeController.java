e4f1bfb7d93c2eed3bf59836ac1b58da30f80a81
gperson <gperson@xpanxion.com>
2014-01-05 20:22:55 -0600

    Password encryption, other framework changes.

--- src/main/java/com/xpanxion/skeleton/controllers/HomeController.java
+++ src/main/java/com/xpanxion/skeleton/controllers/HomeController.java
@@ -3,7 +3,9 @@ package com.xpanxion.skeleton.controllers;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.xpanxion.skeleton.service.TestService;
@@ -20,9 +22,9 @@ public class HomeController {
     private TestService testService;
 
     /**
-     * The default controller action for the homepage. 
+     * The default controller action for the homepage.
      * 
-     * @return the Model and View for the home page. 
+     * @return the Model and View for the home page.
      */
     @RequestMapping("**/home")
     public ModelAndView getHomePage() {
@@ -31,10 +33,43 @@ public class HomeController {
         return mAndV;
     }
 
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login(ModelMap model) {
+        ModelAndView mAndV = new ModelAndView("login");
+        return mAndV;
+
+    }
+
+    // @RequestMapping(value = "/home", method = RequestMethod.GET)
+    // public String printWelcome(ModelMap model, Principal principal) {
+    //
+    // String name = principal.getName();
+    // model.addAttribute("username", name);
+    // model.addAttribute("message", "Spring Security Custom Form example");
+    // return "home";
+    //
+    // }
+    //
+    // @RequestMapping(value="/loginfailed", method = RequestMethod.GET)
+    // public String loginerror(ModelMap model) {
+    //
+    // model.addAttribute("error", "true");
+    // return "login";
+    //
+    // }
+    //
+    // @RequestMapping(value="/logout", method = RequestMethod.GET)
+    // public String logout(ModelMap model) {
+    //
+    // return "login";
+    //
+    // }
+
     /**
      * Sets the service for this controller
      * 
-     * @param service the service to use in this controller. 
+     * @param service
+     *            the service to use in this controller.
      */
     @Resource
     public void setTestService(TestService service) {
--- src/main/webapp/jsp/login.jsp
+++ src/main/webapp/jsp/login.jsp
@@ -1,28 +1,29 @@
-
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-	pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ taglib uri='http://java.sun.com/jsp/jstl/core' prefix='c'%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
 <html>
-<head>
-<meta charset="utf-8">
-<title>Login Page</title>
-<meta name="description" content="Login Page">
-<meta name="author" content="Grant Person">
-<!--<link rel="stylesheet" type="text/css" href="styles.css">-->
-</head>
+<head></head>
 <body>
-	<form action="Lobby" method="post" id="login">
-		<h1>Login</h1>
-		<fieldset id="inputs">
-			<input id="username" name="userName" type="text" placeholder="Username" autofocus
-				required> <input id="password" name="password" type="password"
-				placeholder="Password" required>
-		</fieldset>
-		<fieldset>
-			<input type="submit" id="submit" value="Login"> <a href="">Forgot
-				password?</a>
-		</fieldset>
-	</form>
-
+   <h1>Please Login</h1>
+   <c:if test="${'fail' eq param.auth}">
+        <div style="color:red">
+                Login Failed!!!<br />
+                Reason : ${sessionScope["SPRING_SECURITY_LAST_EXCEPTION"].message}
+         </div>
+    </c:if>
+   <form name='f' action="j_spring_security_check" method='POST'>
+      <table>
+         <tr>
+            <td>User:</td>
+            <td><input type='text' name='j_username' value=''></td>
+         </tr>
+         <tr>
+            <td>Password:</td>
+            <td><input type='password' name='j_password' /></td>
+         </tr>
+         <tr>
+            <td><input name="submit" type="submit" value="submit" /></td>
+         </tr>
+      </table>
+  </form>
 </body>
 </html>
